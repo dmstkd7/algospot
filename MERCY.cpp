7d41809be9c7d62b6a6e8ba75708092e76ec7fcd
dmstkd7 <eeunsang7@naver.com>
2016-05-07 21:53:09 +0900

    Create MERCY.cpp

---
+++ MERCY.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+
+int main(void){
+
+	
+	int Test_Case;
+
+
+	scanf("%d", &Test_Case);
+	for (int i = 1; i <= Test_Case; ++i){
+		printf("Hello Algospot!\n");
+	}
+
+	return 0;
+}
